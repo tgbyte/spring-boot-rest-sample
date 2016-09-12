77add897b522087a86ac02fed85f264428a4886c
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-09-12 17:02:35 +0200

    MySQL & autoconfig support

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,9 +1,15 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
